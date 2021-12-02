a68ea2aea26d819cedf41a2e4fbba2f34c1447ed
Masaki Nakano <admin@namachan10777.dev>
2021-12-02 21:11:42 +0800

    format

--- otynium.rb
+++ otynium.rb
@@ -197,16 +197,16 @@ def vscode_colorscheme(name, palette, type)
       "keyword.other.substitution"
 		]
     token 'Block Level Variables, Other Variables, String Link, Tag', :Tag, [
-				"entity.name.tag",
-				"meta.tag.sgml",
-				"markup.deleted.git_gutter"
+      "entity.name.tag",
+      "meta.tag.sgml",
+      "markup.deleted.git_gutter"
     ]
     token 'Function, Special Method', :Function, [
       'entity.name.function', 
-			"meta.function-call",
-			"variable.function",
-			"support.function",
-			"keyword.other.special-method"
+      "meta.function-call",
+      "variable.function",
+      "support.function",
+      "keyword.other.special-method"
     ]
     token 'Number', :Number, ['constant.numeric']
     token 'Constant', :Constant, ['constant.language', 'support.constant']
@@ -240,12 +240,12 @@ def vscode_colorscheme(name, palette, type)
       "support.type"
     ]
     token 'CSS Class and Support', :Typedef, [
-				"source.css support.type.property-name",
-				"source.sass support.type.property-name",
-				"source.scss support.type.property-name",
-				"source.less support.type.property-name",
-				"source.stylus support.type.property-name",
-				"source.postcss support.type.property-name",
+      "source.css support.type.property-name",
+      "source.sass support.type.property-name",
+      "source.scss support.type.property-name",
+      "source.less support.type.property-name",
+      "source.stylus support.type.property-name",
+      "source.postcss support.type.property-name",
     ]
   end
   vscode.output
@@ -255,97 +255,97 @@ end
 def vim_colorscheme(name, palette)
   vim = Vim.new(name, palette)
   vim.instance_eval do
-    hi 'Normal', :Normal
-    hi 'NonText', :NonText
-    hi 'Cursor', :Cursor
-    hi 'CursorLine', :CursorLine
-    hi 'CursorLineNr', :CursorLineNr
-    hi 'CursorColumn', :CursorColumn
-    hi 'LineNr', :LineNr
-    hi 'StatusLine', :StatusLine
-    hi 'StatusLineNC', :StatusLineNC
-    hi 'StatusLineTerm', :StatusLineTerm
-    hi 'StatusLineTermNC', :StatusLineTermNC
-    hi 'Title', :Title
-    hi 'Directory', :Directory
-    hi 'Pmenu', :Pmenu
-    hi 'PmenuSel', :PmenuSel
-    hi 'PmenuSbar', :PmenuSbar
-    hi 'PmenuThumb', :PmenuThumb
-    hi 'Search', :Search
-    hi 'IncSearch', :IncSearch
-    hi 'DiffAdd', :DiffAdd
-    hi 'DiffChange', :DiffChange
-    hi 'DiffDelete', :DiffDelete
-    hi 'DiffText', :DiffText
-    hi 'SpellBad', :SpellBad
-    hi 'SpellCap', :SpellCap
-    hi 'SpellLocal', :SpellLocal
-    hi 'SpellRare', :SpellRare
-    hi 'SpecialKey', :SpecialKey
-    hi 'WildMenu', :WildMenu
-    hi 'Folded', :Folded
-    hi 'FoldColumn', :FoldColumn
-    hi 'VertSplit', :VertSplit
-    hi 'TabLine', :TabLine
-    hi 'TabLineFill', :TabLineFill
-    hi 'TabLineSel', :TabLineSel
-    hi 'ColorColumn', :ColorColumn
-    hi 'SignColumn', :SignColumn
-    hi 'Question', :Question
-    hi 'Visual', :Visual
-    hi 'VisualNOS', :VisualNOS
-    hi 'ModeMsg', :ModeMsg
-    hi 'MoreMsg', :MoreMsg
-    hi 'WarningMsg', :WarningMsg
-    hi 'ErrorMsg', :ErrorMsg
+    hi 'Normal'           , :Normal
+    hi 'NonText'          , :NonText
+    hi 'Cursor'           , :Cursor
+    hi 'CursorLine'       , :CursorLine
+    hi 'CursorLineNr'     , :CursorLineNr
+    hi 'CursorColumn'     , :CursorColumn
+    hi 'LineNr'           , :LineNr
+    hi 'StatusLine'       , :StatusLine
+    hi 'StatusLineNC'     , :StatusLineNC
+    hi 'StatusLineTerm'   , :StatusLineTerm
+    hi 'StatusLineTermNC' , :StatusLineTermNC
+    hi 'Title'            , :Title
+    hi 'Directory'        , :Directory
+    hi 'Pmenu'            , :Pmenu
+    hi 'PmenuSel'         , :PmenuSel
+    hi 'PmenuSbar'        , :PmenuSbar
+    hi 'PmenuThumb'       , :PmenuThumb
+    hi 'Search'           , :Search
+    hi 'IncSearch'        , :IncSearch
+    hi 'DiffAdd'          , :DiffAdd
+    hi 'DiffChange'       , :DiffChange
+    hi 'DiffDelete'       , :DiffDelete
+    hi 'DiffText'         , :DiffText
+    hi 'SpellBad'         , :SpellBad
+    hi 'SpellCap'         , :SpellCap
+    hi 'SpellLocal'       , :SpellLocal
+    hi 'SpellRare'        , :SpellRare
+    hi 'SpecialKey'       , :SpecialKey
+    hi 'WildMenu'         , :WildMenu
+    hi 'Folded'           , :Folded
+    hi 'FoldColumn'       , :FoldColumn
+    hi 'VertSplit'        , :VertSplit
+    hi 'TabLine'          , :TabLine
+    hi 'TabLineFill'      , :TabLineFill
+    hi 'TabLineSel'       , :TabLineSel
+    hi 'ColorColumn'      , :ColorColumn
+    hi 'SignColumn'       , :SignColumn
+    hi 'Question'         , :Question
+    hi 'Visual'           , :Visual
+    hi 'VisualNOS'        , :VisualNOS
+    hi 'ModeMsg'          , :ModeMsg
+    hi 'MoreMsg'          , :MoreMsg
+    hi 'WarningMsg'       , :WarningMsg
+    hi 'ErrorMsg'         , :ErrorMsg
     # syntax highlighting
-    hi 'Constant', :Constant
-    hi 'String', :String
-    hi 'Character', :Character
-    hi 'Number', :Number
-    hi 'Float', :Float
-    hi 'Boolean', :Boolean
-    hi 'Identifier', :Identifier
-    hi 'Function', :Function
-    hi 'Statement', :Statement
-    hi 'Conditional', :Conditional
-    hi 'Repeat', :Repeat
-    hi 'Label', :Label
-    hi 'Operator', :Operator
-    hi 'Keyword', :Keyword
-    hi 'Exception', :Exception
-    hi 'Type', :Type
-    hi 'StorageClass', :StorageClass
-    hi 'Structure', :Structure
-    hi 'Typedef', :Typedef
-    hi 'PreProc', :PreProc
-    hi 'Include', :Include
-    hi 'Define', :Define
-    hi 'Macro', :Macro
-    hi 'PreCondit', :PreCondit
-    hi 'Special', :Special
-    hi 'SpecialChar', :SpecialChar
-    hi 'Tag', :Tag
-    hi 'Delimiter', :Delimiter
-    hi 'SpecialComment', :SpecialComment
-    hi 'Debug', :Debug
-    hi 'Underlined', :Underlined
-    hi 'Comment', :Comment
-    hi 'Todo', :Todo
-    hi 'Ignore', :Ignore
-    hi 'Error', :Error
-    hi 'MatchParen', :MatchParen
-    hi 'Warning', :Warning
-    hi 'hint', :hint
-    hi 'hintMsg', :hintMsg
-    hi 'Info', :Info
-    hi 'InfoMsg', :InfoMsg
+    hi 'Constant'       , :Constant
+    hi 'String'         , :String
+    hi 'Character'      , :Character
+    hi 'Number'         , :Number
+    hi 'Float'          , :Float
+    hi 'Boolean'        , :Boolean
+    hi 'Identifier'     , :Identifier
+    hi 'Function'       , :Function
+    hi 'Statement'      , :Statement
+    hi 'Conditional'    , :Conditional
+    hi 'Repeat'         , :Repeat
+    hi 'Label'          , :Label
+    hi 'Operator'       , :Operator
+    hi 'Keyword'        , :Keyword
+    hi 'Exception'      , :Exception
+    hi 'Type'           , :Type
+    hi 'StorageClass'   , :StorageClass
+    hi 'Structure'      , :Structure
+    hi 'Typedef'        , :Typedef
+    hi 'PreProc'        , :PreProc
+    hi 'Include'        , :Include
+    hi 'Define'         , :Define
+    hi 'Macro'          , :Macro
+    hi 'PreCondit'      , :PreCondit
+    hi 'Special'        , :Special
+    hi 'SpecialChar'    , :SpecialChar
+    hi 'Tag'            , :Tag
+    hi 'Delimiter'      , :Delimiter
+    hi 'SpecialComment' , :SpecialComment
+    hi 'Debug'          , :Debug
+    hi 'Underlined'     , :Underlined
+    hi 'Comment'        , :Comment
+    hi 'Todo'           , :Todo
+    hi 'Ignore'         , :Ignore
+    hi 'Error'          , :Error
+    hi 'MatchParen'     , :MatchParen
+    hi 'Warning'        , :Warning
+    hi 'hint'           , :hint
+    hi 'hintMsg'        , :hintMsg
+    hi 'Info'           , :Info
+    hi 'InfoMsg'        , :InfoMsg
     # coc.nvim
-    hi 'CocErrorhighlight', :CocErrorhighlight
-    hi 'CocWarninghighlight', :CocWarninghighlight
-    hi 'Cochinthighlight', :Cochinthighlight
-    hi 'CocInfohighlight', :CocInfohighlight
+    hi 'CocErrorhighlight'   , :CocErrorhighlight
+    hi 'CocWarninghighlight' , :CocWarninghighlight
+    hi 'Cochinthighlight'    , :Cochinthighlight
+    hi 'CocInfohighlight'    , :CocInfohighlight
     # ALE
     link 'ALEErrorSign'   , 'ErrorMsg'
     link 'ALEWarningSign' , 'WarningMsg'
@@ -403,69 +403,69 @@ def vim_colorscheme(name, palette)
     link 'TSTypeBuiltin'        , 'Type'
     link 'TSStructure'          , 'Structure'
     link 'TSInclude'            , 'Include'
-    hi 'TSVariable', :TSVariable
+    hi 'TSVariable'             , :TSVariable
     link 'TSVariableBuiltin'    , 'TSVariable'
     link 'TSText'               , 'Normal'
-    hi 'TSStrong', :TSStrong
+    hi 'TSStrong'               , :TSStrong
     link 'TSEmphasis'           , 'TSStrong'
-    hi 'TSStrong', :TSStrong
+    hi 'TSStrong'               , :TSStrong
     link 'TSTitle'              , 'Title'
-    hi 'TSLiteral', :TSLiteral
-    hi 'TSURI', :TSURI
+    hi 'TSLiteral'              , :TSLiteral
+    hi 'TSURI'                  , :TSURI
     link 'TSTag'                , 'Tag'
-    hi 'TSTagDelimiter', :TSTagDelimiter
+    hi 'TSTagDelimiter'         , :TSTagDelimiter
     # html and markdown
-    hi 'markdownH1', :markdownH1
-    hi 'markdownH2', :markdownH2
-    hi 'markdownH3', :markdownH3
-    hi 'markdownH4', :markdownH4
-    hi 'markdownH5', :markdownH5
-    hi 'markdownH6', :markdownH6
-    hi 'markdownBold', :markdownBold
-    hi 'markdownItalic', :markdownItalic
-    hi 'markdownBoldItalic', :markdownBoldItalic
+    hi 'markdownH1'         , :markdownH1
+    hi 'markdownH2'         , :markdownH2
+    hi 'markdownH3'         , :markdownH3
+    hi 'markdownH4'         , :markdownH4
+    hi 'markdownH5'         , :markdownH5
+    hi 'markdownH6'         , :markdownH6
+    hi 'markdownBold'       , :markdownBold
+    hi 'markdownItalic'     , :markdownItalic
+    hi 'markdownBoldItalic' , :markdownBoldItalic
   end
   vim.output
 end
 
 color = {
-  red: "\#ef7070",
-  red2: "\#8a1a1a",
-  orange: "\#e8a368",
-  yellow: "\#d8cf6c",
+  red:     "\#ef7070",
+  red2:    "\#8a1a1a",
+  orange:  "\#e8a368",
+  yellow:  "\#d8cf6c",
   yellow2: "\#e2d86a",
-  green: "\#85c178",
-  green2: "\#5cc46b",
-  cyan: "\#64bac9",
-  blue: "\#6882e2",
-  purple: "\#b281d1",
-  gray1: "\#222c33",
-  gray2: "\#314047",
-  gray3: "\#506872",
-  gray4: "\#5f7b87",
-  gray5: "\#7395a3",
-  white: "\#a9afb2",
-  black: "\#050a26"
+  green:   "\#85c178",
+  green2:  "\#5cc46b",
+  cyan:    "\#64bac9",
+  blue:    "\#6882e2",
+  purple:  "\#b281d1",
+  gray1:   "\#222c33",
+  gray2:   "\#314047",
+  gray3:   "\#506872",
+  gray4:   "\#5f7b87",
+  gray5:   "\#7395a3",
+  white:   "\#a9afb2",
+  black:   "\#050a26",
 }
 
 color256 = {
-  red: "\#d75f5f",
-  red2: "\#870000",
-  orange: "\#ffaf5f",
-  yellow: "\#d7d75f",
+  red:     "\#d75f5f",
+  red2:    "\#870000",
+  orange:  "\#ffaf5f",
+  yellow:  "\#d7d75f",
   yellow2: "\#d7d700",
-  green: "\#87af87",
-  green2: "\#5faf5f",
-  cyan: "\#5fafd7",
-  blue: "\#5f87ff",
-  purple: "\#af87ff",
-  gray1: "\#262626",
-  gray2: "\#303030",
-  gray3: "\#4e4e4e",
-  gray4: "\#585858",
-  gray5: "\#767676",
-  white: "\#a8a8a8",
-  black: "\#00005f"
+  green:   "\#87af87",
+  green2:  "\#5faf5f",
+  cyan:    "\#5fafd7",
+  blue:    "\#5f87ff",
+  purple:  "\#af87ff",
+  gray1:   "\#262626",
+  gray2:   "\#303030",
+  gray3:   "\#4e4e4e",
+  gray4:   "\#585858",
+  gray5:   "\#767676",
+  white:   "\#a8a8a8",
+  black:   "\#00005f",
 }
 
 def palette(color)
